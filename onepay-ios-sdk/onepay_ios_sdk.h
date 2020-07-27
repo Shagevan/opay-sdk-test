df9e5da5d2fe661f2ae869120c58ac0f51440200
Parameswaran Shagevan <parameswaran.shagevan@singtel.com>
2020-07-27 10:32:13 +0800

    Initial Commit

---
+++ onepay-ios-sdk/onepay_ios_sdk.h
@@ -0,0 +1,19 @@
+//
+//  onepay_ios_sdk.h
+//  onepay-ios-sdk
+//
+//  Created by ShagevanParameswaran on 27/7/20.
+//  Copyright © 2020 ShagevanParameswaran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for onepay_ios_sdk.
+FOUNDATION_EXPORT double onepay_ios_sdkVersionNumber;
+
+//! Project version string for onepay_ios_sdk.
+FOUNDATION_EXPORT const unsigned char onepay_ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <onepay_ios_sdk/PublicHeader.h>
+
+
